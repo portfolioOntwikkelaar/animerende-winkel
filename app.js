3de63eb29c79b027272974df039291a559dca2a8
noordseoostende <voropayev1@mail.ru>
2022-02-16 15:11:29 +0100

    nog Enter Animatie toevoegen

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+const tlLeave = gsap.timeline({
+  defaults: {duration: 0.75, ease: "Power2.easeOut"},
+
+});
+const tlEnter = gsap.timeline({
+  defaults: {duration: 0.75, ease: "Power2.easeOut"},
+
+});
+
+const leaveAnimation = (current, done) => {
+  const product = current.querySelector('.image-container');
+  const text = current.querySelector('.showcase-text');
+  const circles = current.querySelectorAll('.circle');
+  const arrow = current.querySelector('.showcase-arrow');
+  return(
+    tlLeave.fromTo(arrow, {opacity: 1, y:0}, {opacity: 0, y:50}),
+    tlLeave.fromTo(product, {y:0, opacity:1}, { y:100, opacity: 0, onComplete: done}, "<"),
+    tlLeave.fromTo(text, {y:0, opacity:1}, {opacity:0, y:100}, "<")
+    // tlLeave.to(current, {zIndex: 1}, '<');
+
+  )
+};
 barba.init({
   preventRunning: true,
   transitions: [
@@ -6,7 +28,8 @@ barba.init({
       leave(data){
         const done = this.async();
         let current = data.current.container;
-        gsap.fromTo(current, {opacity: 1}, {opacity: 0, duration: 1, onComplete: done})
+        leaveAnimation(current, done)
+        // gsap.fromTo(current, {opacity: 1}, {opacity: 0, duration: 1, onComplete: done})
       },
       enter(data){
         const done = this.async();
